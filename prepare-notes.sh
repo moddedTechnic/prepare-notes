80ee0aa826d82957c37be8442fbd97ddca3fed8b
Jonathan Leeming <jpleeming51@gmail.com>
2022-10-27 19:07:44 +0100

    Initial commit

---
+++ prepare-notes.sh
@@ -0,0 +1,34 @@
+downloads=$(./get-downloads.sh)
+
+target=$1
+shift 1
+
+if [ ! -f $target/notes.md ]
+then
+	echo "Preparing directory"
+	[ -d $target ] && rm -r $target
+	mkdir $target/{,img,slides}
+
+	echo "Moving presentations"
+	i=1
+	for pattern in "$@"
+	do
+		for slidesfile in $downloads/$pattern.pdf
+		do
+			num=$(printf "%02d" $i)
+			cp -T "$slidesfile" $target/slides/slides-$num.pdf
+			i=$((i + 1))
+		done
+	done
+
+	echo "Combining presentations"
+	pdfunite $target/slides/slides-*.pdf $target/slides/slides.pdf
+
+	echo "Extracting slides"
+	pdftoppm -jpeg $target/slides/slides.pdf $target/img/slide
+
+	echo "Constructing notes"
+	ls -l $target/img | grep -oE 'slide-[0-9]{1,}\.jpg' | sed 's/.*/\!\[\]\(\.\/img\/&\)/g' > $target/notes.md
+fi
+
+code $target/notes.md
